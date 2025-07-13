89da36e74304cc9065d1e048d0fa029a25d3bb3e
Minh Hong <nguyenminhhong2061@gmail.com>
2025-07-13 15:38:28 +0700

    relative absolute import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Board from "./pages/Boards/_id"
+import Board from "~/pages/Boards/_id"
 
 function App() {
 
   return (
     <>
-      // React Router DOM
-      // Board Details
       <Board />
     </>
   )
